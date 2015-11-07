474c5cb2b9e5b354885e81aa2566b7c224e95c22
Tomas Valent <equivalent@eq8.eu>
2015-11-07 12:47:57 +0000

    remove faling spec

--- spec/reading_log_extractor_spec.rb
+++ spec/reading_log_extractor_spec.rb
@@ -4,8 +4,4 @@ describe ReadingLogExtractor do
   it 'has a version number' do
     expect(ReadingLogExtractor::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
