d1ab4f38e8f60c44377a59e76b4afe8b6c852e93
Tomas Valent <equivalent@eq8.eu>
2015-11-07 14:12:55 +0000

    enable code climate in VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr"
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
   config.configure_rspec_metadata!
 end
 
